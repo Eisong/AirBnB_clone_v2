b590fb4f9905c46c8dccb5f482fb81fbec3bb317
Edet Isong <eddieisong1@gmail.com>
2023-12-07 02:27:34 -0800

    AirBnB Project on 0x03. AirBnB clone - Deploy static by Edet Isong

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['52.55.249.213', '54.157.32.137']
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
