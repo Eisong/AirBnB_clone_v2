5473107e8b91ac140e17549b2bad8b8967dbbed3
Edet Isong <eddieisong1@gmail.com>
2023-12-29 04:42:18 -0800

    My AirBnB Project on 0x04. AirBnB clone - Web framework by Edet Isong

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Define the route for the root URL '/'
 @app.route('/', strict_slashes=False)
-def hello hbnb();
+def hello_hbnb();
 	""" Displays 'Hello HBNB! """
 	return "Hello HBNB!"
 
