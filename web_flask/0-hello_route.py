4692a4654c8f64a6f6b0e1e2f8dd277ebc0ac569
Edet Isong <eddieisong1@gmail.com>
2023-12-29 05:28:09 -0800

    My AirBnB Project on 0x04. AirBnB clone - Web framework by Edet Isong

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""Starts a Flask web application
-"""
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,7 +9,7 @@ def hello_hbnb();
 	""" Displays 'Hello HBNB!' """
 	return "Hello HBNB!"
 
-    if __name__ == "__main__":
+if __name__ == '__main__':
 	# Start the Flask development server
 	# Listen on all available network interfaces (0.0.0.0) and port 5000
 	app.run(host='0.0.0.0', port=5000)
